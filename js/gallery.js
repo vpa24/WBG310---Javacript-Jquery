387512fc56cc5dee457f227953f8769502da7cde
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-02-11 19:52:58 -0800

    add link to each place title

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
-  var places_name = [
-    "Halong Bay – Bay of Descending Dragons",
-    "Son Tra Peninsula",
-    "Phu Quoc Island",
-    "Tram Ton Pass",
-    "Mekong Delta",
-    "Phong Nha-Ke Bang National Park",
-    "Ghenh Da Dia",
+  var places = [
+    {
+      name: "Halong Bay – Bay of Descending Dragons",
+      link: "https://en.wikipedia.org/wiki/H%E1%BA%A1_Long_Bay",
+    },
+    {
+      name: "Son Tra Peninsula",
+      link: "https://en.wikipedia.org/wiki/S%C6%A1n_Tr%C3%A0_Mountain",
+    },
+    {
+      name: "Phu Quoc Island",
+      link: "https://en.wikipedia.org/wiki/Ph%C3%BA_Qu%E1%BB%91c",
+    },
+    {
+      name: "Mekong Delta",
+      link: "https://en.wikipedia.org/wiki/Mekong_Delta",
+    },
+    {
+      name: "Tram Ton Pass",
+      link: "https://localvietnam.com/blog/tram-ton-pass/",
+    },
   ];
   var oldIndex, index;
   $("#gallery img").click(function () {
@@ -16,7 +29,7 @@ $(document).ready(function () {
     var imgPath = $(this).attr("src");
     var largeImgPath = imgPath.replace("small", "large");
     var newElement = `<div class="slideshow_${index}">
-		<h2>${places_name[index]}</h2><img src="${largeImgPath}" />
+		<h2><a href=${places[index]["link"]}>${places[index]["name"]}</a></h2><img src="${largeImgPath}" />
 	</div>`;
     $(newElement).hide();
     $("#photo").prepend(newElement);
