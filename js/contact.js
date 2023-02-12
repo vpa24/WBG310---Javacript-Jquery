350a21988716c50f478436d0f38677cd8a9877ce
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-02-12 14:32:11 -0800

    change validate form

--- js/contact.js
+++ js/contact.js
@@ -1,140 +1,48 @@
-$(function () {
-  var fullNameError,
-    emailError,
-    ageError,
-    phoneError,
-    contactError,
-    commentError = false;
-  $("#success_message").hide();
-
-  // validate Full Name
-  $("#namecheck").hide();
-  $("#fullName").keyup(function () {
-    validateName();
+$(document).ready(function () {
+  $("#contact").validate({
+    rules: {
+      age: { required: true, number: true, maxlength: 2 },
+      email: { required: true, email: true },
+      phone: {
+        required: true,
+        phoneUS: true,
+      },
+    },
+    messages: {
+      age: {
+        required: "Please enter your age",
+        number: "You must type only number",
+        maxlength: "Age must less than 100",
+      },
+      email: {
+        required: "Please supply your e-mail address.",
+        email: "This is not a valid e-mail address.",
+      },
+      phone: {
+        required: "Please enter your phone number",
+      },
+    },
+    errorPlacement: function (error, element) {
+      error.insertAfter(element);
+    },
+    submitHandler: function (form) {
+      // for demo
+      alert("Thanks for keep in touch!");
+      return false;
+    },
   });
-  function validateName() {
-    let nameVal = $("#fullName").val();
-    if (nameVal.length == "") {
-      $("#namecheck").show();
-      fullNameError = true;
-    } else {
-      $("#namecheck").hide();
-      fullNameError = false;
-    }
-  }
-  // validate age
-  $("#agecheck").hide();
-  $("#age").keyup(function () {
-    validateAge();
-  });
-  function validateAge() {
-    let ageVal = $("#age").val();
-    if (ageVal.length == "") {
-      $("#agecheck").show();
-      ageError = true;
-    } else if (!$.isNumeric(ageVal)) {
-      $("#agecheck").html("Your age must be a number!");
-      $("#agecheck").show();
-      ageError = true;
-    } else {
-      $("#agecheck").hide();
-      ageError = false;
-    }
-  }
-
-  // validate phone
-  $("#phonecheck").hide();
-  $("#phone").keyup(function () {
-    validatePhone();
-  });
-  function validatePhone() {
-    var phoneVal = $("#phone").val();
-    var filter =
-      /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
-    if (phoneVal.length == "") {
-      $("#phonecheck").show();
-      phoneError = true;
-    } else if (!filter.test(phoneVal)) {
-      $("#phonecheck").html(
-        "The number you passed is not a valid US phone number"
+  jQuery.validator.addMethod(
+    "phoneUS",
+    function (phone_number, element) {
+      phone_number = phone_number.replace(/\s+/g, "");
+      return (
+        this.optional(element) ||
+        (phone_number.length > 9 &&
+          phone_number.match(
+            /^(\+?1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/
+          ))
       );
-      $("#phonecheck").show();
-    } else {
-      $("#phonecheck").hide();
-      phoneError = false;
-    }
-  }
-
-  // validate email
-  $("#emailcheck").hide();
-  $("#email").keyup(function () {
-    validateEmail();
-  });
-  function validateEmail() {
-    var emailVal = $("#email").val();
-    var filter = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
-    if (emailVal.length == "") {
-      $("#emailcheck").show();
-      emailError = true;
-    } else if (!filter.test(emailVal)) {
-      $("#emailcheck").html("It is not a valid e-mail address");
-      $("#emailcheck").show();
-    } else {
-      $("#emailcheck").hide();
-      emailError = false;
-    }
-  }
-
-  // validate comment
-  $("#commentcheck").hide();
-  $("#comment").keyup(function () {
-    validateComment();
-  });
-  function validateComment() {
-    var commentVal = $("#comment").val();
-    if (commentVal.length == "") {
-      $("#commentcheck").show();
-      commentError = true;
-    } else if (commentVal.length < 20) {
-      $("#commentcheck").html(
-        "Please write your message greater than 20 characters"
-      );
-      $("#commentcheck").show();
-    } else {
-      $("#commentcheck").hide();
-      commentError = false;
-    }
-  }
-  // validate contact method
-  $("#radiocheck").hide();
-  function validateContactMethod() {
-    var contactVal = $("input[name='contact']:checked").val();
-    if (!contactVal) {
-      $("#radiocheck").show();
-      contactError = true;
-    } else {
-      $("#radiocheck").hide();
-      contactError = false;
-    }
-  }
-  $("#contact-form").click(function () {
-    validateName();
-    validateAge();
-    validatePhone();
-    validateEmail();
-    validateComment();
-    validateContactMethod();
-    if (
-      fullNameError == false &&
-      ageError == false &&
-      phoneError == false &&
-      emailError == false &&
-      commentError == false &&
-      contactError == false
-    ) {
-      $("#contact").trigger("reset");
-      $("#success_message").show();
-      return;
-    }
-  });
-});
+    },
+    "Please specify a valid phone number"
+  );
+}); // end ready
