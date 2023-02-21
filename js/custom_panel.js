673d788ddeaee32ef1f2ed5a2e0cd8811fe4c811
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-02-20 18:50:01 -0800

    change dynamic border color

--- js/custom_panel.js
+++ js/custom_panel.js
@@ -2,6 +2,21 @@ $(document).ready(function () {
   $("#tabContainer").tabs({
     show: "fadeIn",
     hide: "fadeOut",
+    activate: function (event, ui) {
+      console.log(ui);
+      const awesome_boder = $(".awesome-border");
+      console.log();
+      // if that card has no colours, add some
+      // if (!awesome_boder.css(`--color`)) {
+      awesome_boder.css(`--color1`, `hsl(${Math.random() * 360}, 100%, 50%)`);
+      awesome_boder.css(`--color2`, `hsl(${Math.random() * 360}, 100%, 50%)`);
+
+      // card.style.setProperty(
+      //   `--color2`,
+      //   `hsl(${Math.random() * 360}, 100%, 50%)`
+      // );
+      // }
+    },
   });
   var hash = location.hash;
   if (hash) {
